8132fa3aaf3e3e02c6970d1ff4d5ef4824f4b8c4
Jonathan Grant <jonathanallengrant@gmail.com>
2019-11-12 14:08:55 +0000

    'Finally got db stuff to work'

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 import sqlite3
+from collections import defaultdict
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-DB_FILE = "data.db"
+DB_FILE = "/home/jon/covfefe/backend/data.db"
 FLAVORS = [
     "Elvazio",
     "etc"
@@ -20,12 +21,16 @@ def vote():
         :param int score: Score from (0-5).
         :param str note: Note to add to rating (optional).
     """
-    flavor, score, note = request.json["flavor"], request.json["score"], request.json.get("note", "")
+    data = request.get_json(force=True)
+    flavor, score, note = data["flavor"], data["score"], data.get("note", "")
     conn = sqlite3.connect(DB_FILE)
     command = "INSERT INTO ratings(flavor,score,note) VALUES(?,?,?)"
     cursor = conn.cursor()
     cursor.execute(command, (flavor, score, note))
+    conn.commit()
+    cursor.close()
     conn.close()
+    print("Inserted {}, {}, {}".format(flavor, score, note))
     return jsonify({"status": "Success!"})
 
 
@@ -33,6 +38,26 @@ def vote():
 def get_flavors():
     """Return all flavors for ratings."""
     return jsonify(FLAVORS)
+
+
+@app.route("/ratings")
+def get_ratings():
+    """Return all ratings in database. Frontend can handle averages, etc."""
+    conn = sqlite3.connect(DB_FILE)
+    command = "SELECT * FROM ratings"
+    cursor = conn.cursor()
+    cursor.execute(command)
+    all_rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    # Turn rows into dictionary where key is coffee name and values are ratings
+    ratings = defaultdict(list)
+    for row in all_rows:
+        ratings[row[0]].append({"score": row[1], "note": row[2]})
+
+    return jsonify(ratings)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
